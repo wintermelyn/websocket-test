6d748ea44b77b5e242ec394e4b6c8d389b71aeb1
wintermelyn <matas.seplveda@uc.cl>
2024-07-04 11:28:42 -0400

    base

---
+++ index.js
@@ -0,0 +1,9 @@
+const WebSocket = require('ws')
+const PORT = process.env.PORT || 3000;
+const wss = new WebSocket.Server({ port: PORT })
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log(`Received message => ${message}`)
+  })
+  setInterval(function () {ws.send('Hello! Message From Server!!')}, 1000);
+})
